02a326d69dfb0ba5a4ec2c6d5d57e56a60bf4921
Vinodkumar N <24507@ITCINFOTECH.COM>
2017-04-13 13:20:43 +0530

    testing header

---
+++ app/header/header.component.js
@@ -0,0 +1,6 @@
+myHeader.component('myHeader',{
+  controller: function myHeaderCtrl() {
+
+  },
+  templateUrl: 'app/header/header.html'
+})
---
+++ app/header/header.module.js
@@ -0,0 +1,1 @@
+var myHeader = angular.module('myHeader',[]);
