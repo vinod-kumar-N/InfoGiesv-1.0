2974caa21532fe5af413f64f64298f099b5510e5
Vinodkumar N <24507@ITCINFOTECH.COM>
2017-04-18 11:27:08 +0530

    adding content

--- app/controllers/contactController.js
+++ app/controllers/contactController.js
@@ -2,4 +2,15 @@ infoGies.controller('contactController', function(){
     if($('body').hasClass('moveMe')){
         $('.menu__handle').trigger('click');
     }
+        var callResizer = function callResizer(){
+                if($(window).width() < 992){
+                    $('.contactContainer').addClass('mobile');
+                } else {
+                     $('.contactContainer').removeClass('mobile');
+                }
+            return callResizer;
+        }();
+    $(window).on('resize', function(){
+        callResizer();
+    })
 })
